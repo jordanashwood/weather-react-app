7d7bdbad2c5f0680f86d76ff13f0e68e26a7c1c7
jordanashwood <jordanmace0124@gmail.com>
2023-10-30 10:53:46 -0400

    Added axios integration to display weather

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°F`
+    );
+  }
+  let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <ClipLoader color="#74f396" loading="true" size={100} />;
+}
